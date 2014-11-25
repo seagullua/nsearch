697979fce2a6d05e06bf31a0346b5158f1eb57a5
Andrii Chaika <andriy@chaika.in.ua>
2014-11-25 17:41:32 +0200

    Clusterisation

--- cluster.js
+++ cluster.js
@@ -2,8 +2,19 @@
  * Created by Andriy on 25.11.2014.
  */
 var cluster = require('./lib/clusterisation');
+var FullIndex = require('./lib/FullIndex');
 
-console.log(cluster.cosineDifference(
-   ["word1", "word2", "ccc", "aa"],
-    ["word1", "word2", "aa", "bbb"]
-));
+var index = FullIndex.indexFromFileSync(process.argv[2]);
+
+var cluster = (cluster.clusterizeIndex(index));
+
+cluster.forEach(function(cluster){
+    console.log("---------------------------------");
+    console.log("Inside: ", cluster.attached.length);
+    console.log("Lead:", cluster.lead);
+    console.log("****");
+
+    cluster.attached.forEach(function(doc){
+        console.log(Math.round(doc.diff*100)/100, doc.document.author, doc.document.title);
+    });
+});
--- find.js
+++ find.js
@@ -1,15 +1,14 @@
 /**
  * Created by Andriy on 25.11.2014.
  */
-var FullIndex = require('./lib/FullIndex').FullIndex;
+var FullIndex = require('./lib/FullIndex');
 var fs = require('fs');
 
-var index = new FullIndex();
+
 
 var index_file = process.argv[2];
 var query = process.argv.slice(3).join(' ');
 
-var json = JSON.parse(fs.readFileSync(index_file, "utf-8"));
-index.loadFromJSON(json);
+var index = FullIndex.indexFromFileSync(index_file);
 
 console.log(JSON.stringify(index.findRanked(query).slice(0, 3), null, 4));
